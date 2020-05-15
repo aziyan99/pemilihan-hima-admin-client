ffde3187b5e8438e11ef9810dc3f0a97daeac6f0
aziyan99 <rajaazian08@gmail.com>
2020-05-15 23:53:28 +0700

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UsersComponent } from './pages/users/users.component';
+import { CandidatesComponent } from './pages/candidates/candidates.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'user', component: UsersComponent },
+  { path: 'candidate', component: CandidatesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
