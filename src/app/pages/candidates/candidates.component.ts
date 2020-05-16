c76565450113c19c6423c6b0ff690243df7311c3
aziyan99 <rajaazian08@gmail.com>
2020-05-16 12:41:34 +0700

    implement operation for candidate

--- src/app/pages/candidates/candidates.component.ts
+++ src/app/pages/candidates/candidates.component.ts
@@ -9,9 +9,73 @@ import { CandidatesService } from 'src/app/services/candidates/candidates.servic
 })
 export class CandidatesComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  btnAdd = true;
+  errorM = false;
+  successM = false;
+  candidateData: any;
+  insertCandidate = {
+    nim: ''
+  };
+
+  constructor(private router: Router, private candidateService: CandidatesService) { }
 
   ngOnInit(): void {
+    if (!localStorage.getItem('_himatoken')) {
+      this.router.navigate(['dashboard']);
+    }
+    this.retriveData();
+  }
+
+  retriveData() {
+    this.candidateService.getAll()
+      .subscribe(data => {
+        this.candidateData = data;
+        this.candidateData = this.candidateData.data;
+      }, error => {
+        console.log(error);
+      })
+  }
+
+  saveData() {
+    this.btnAdd = false;
+    this.loading = true;
+    const data = {
+      nim: this.insertCandidate.nim
+    };
+    this.candidateService.store(data)
+      .subscribe(response => {
+        this.loading = false;
+        this.btnAdd = true;
+        this.successM = true;
+        this.clearForm();
+        this.ngOnInit();
+      }, error => {
+        console.log(error);
+        this.loading = false;
+        this.btnAdd = true;
+        this.errorM = true;
+        this.clearForm();
+      })
+  }
+
+  clearForm() {
+    this.insertCandidate = {
+      nim: ''
+    };
+  }
+
+  deleteData(id) {
+    var check = confirm('Delete this data?');
+    if (check == true) {
+      this.candidateService.destroy(id)
+        .subscribe(res => {
+          this.clearForm();
+          this.ngOnInit();
+        });
+    } else {
+      this.ngOnInit();
+    }
   }
 
 }
